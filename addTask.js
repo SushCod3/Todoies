a07d8399249c8f74dfff5ed13b892a234e9fdd71
imsushmoy <hiraku.kazuma5@gmail.com>
2023-11-14 15:37:42 +0600

    added card for the tasks and a fully functional clear button

--- addTask.js
+++ addTask.js
@@ -2,6 +2,7 @@ const addButton = document.getElementById("addButton");
 const clear = document.getElementById("clear");
 const mainTasks = document.getElementById("mainTasks");
 const inputText = document.getElementById("inputText");
+const card = document.getElementById("card");
 // Create a text node with a space
 const spaceTextNode = document.createTextNode(" ");
 
@@ -37,26 +38,65 @@ clear.addEventListener("click", function () {
   localStorage.removeItem("tasks");
 });
 
-// Adding the task function
+// Function to clear individual task
+function clearIndividual(event) {
+  // finds the closest event the .card
+  const taskToRemove = event.target.closest(".card");
+  if (taskToRemove) {
+    taskToRemove.remove();
+    // Update localStorage after removing the task
+    localStorage.setItem("tasks", mainTasks.innerHTML);
+  }
+}
+// Event delegation for clearing individual tasks
+mainTasks.addEventListener("click", function(event) {
+  if (event.target.classList.contains("btn-danger")) {
+    clearIndividual(event);
+  }
+});
+
 function addTask() {
   // Getting the inputValue from user
   const inputValue = document.getElementById("inputText").value;
-  // Creating new li line
-  const newLi = document.createElement("li");
-  // Checkboxes
-  /* const newCheckbox = document.createElement("input");
-  newCheckbox.type = "checkbox";
-  // Space before checkbox
-  newLi.appendChild(spaceTextNode);
-  newLi.appendChild(newCheckbox); */
-  // storing the value inside newLi
-  newLi.textContent = inputValue;
-  // Appending to ul/mainTasks
-  mainTasks.appendChild(newLi);
-  // Clearning the inputText field
+
+  // Creating a new card element
+  const newCard = document.createElement("div");
+  newCard.classList.add("card", "mb-3");
+
+  // Card body
+  const cardBody = document.createElement("div");
+  // combine the text and button together
+  cardBody.classList.add("card-body", "d-flex", "justify-content-between", "align-items-center");
+
+  // Title
+  const cardTitle = document.createElement("h5");
+  cardTitle.classList.add("card-title");
+  cardTitle.textContent = inputValue;
+
+  // Text-end div
+  const textEndDiv = document.createElement("div");
+  textEndDiv.classList.add("text-end");
+
+  // Clear button
+  const clearBtn = document.createElement("a");
+  clearBtn.href = "#";
+  clearBtn.classList.add("btn", "btn-danger", "btn-sm", "text-end");
+  clearBtn.textContent = "Clear";
+
+  // Constructing the card structure
+  textEndDiv.appendChild(clearBtn);
+  cardBody.appendChild(cardTitle);
+  cardBody.appendChild(textEndDiv);
+  newCard.appendChild(cardBody);
+
+  // Appending the new card to the container
+  mainTasks.appendChild(newCard);
+
+  // Clearing the inputText field
   inputText.value = "";
   // Showing the clear button
   clear.style.visibility = "visible";
-  // Storing the items in localStorage with setItem. innerHTML selects everything inside of a HTML element
+  // Storing the items in localStorage with setItem. innerHTML selects everything inside of an HTML element
   localStorage.setItem("tasks", mainTasks.innerHTML);
 }
+
